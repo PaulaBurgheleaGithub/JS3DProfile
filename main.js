e7ff0613e96f805d40013ac2bcf0110f6b499427
Paula Burghelea <paulaburghelea.github@gmail.com>
2024-04-12 18:57:12 +0100

    Project completed

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 import * as THREE from "three";
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { OrbitControls, ThreeMFLoader } from 'three/examples/jsm/Addons.js';
 
 
 const scene = new THREE.Scene();
@@ -15,11 +15,12 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 camera.position.setZ(30);
+camera.position.setX(-3);
 
 renderer.render( scene, camera);
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xFF6317 });
+const material = new THREE.MeshStandardMaterial({ color: 0xDA9A01 });
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
@@ -30,9 +31,9 @@ pointLigit.position.set(5, 5, 5,);
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLigit, ambientLight);
 
-const lightHelper = new THREE.PointLightHelper(pointLigit);
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper);
+// const lightHelper = new THREE.PointLightHelper(pointLigit);
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
@@ -47,6 +48,57 @@ function addStar() {
 };
 
 Array(300).fill().forEach(addStar);
+
+//scene texture
+
+const sceneTexture = new THREE.TextureLoader().load('/assets/16.webp');
+scene.background = sceneTexture;
+
+//avatar
+
+const avatarTexture = new THREE.TextureLoader().load('/assets/12.webp');
+const profile = new THREE.Mesh(
+  new THREE.BoxGeometry(3,3,3),
+  new THREE.MeshBasicMaterial({map: avatarTexture})
+);
+
+scene.add(profile);
+profile.position.z = -5;
+profile.position.x = 2;
+// ball/sphere
+
+const sphereTexture = new THREE.TextureLoader().load('/assets/22.webp');
+const surfaceTexture = new THREE.TextureLoader().load('/assets/texture.jpg');
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: sphereTexture,
+    normalMap: surfaceTexture
+
+  })
+);
+
+scene.add(sphere);
+sphere.position.z = 30;
+sphere.position.setX(-10);
+
+//scroll animation/positioning
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  sphere.rotation.x += 0.05;
+  sphere.rotation.y += 0.075;
+  sphere.rotation.z += 0.05;
+
+  profile.rotation.y += 0.01;
+  profile.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
 
 function animate() {
   requestAnimationFrame(animate);
